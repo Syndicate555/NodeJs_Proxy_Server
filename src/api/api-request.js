8b2caf884770840ae60f300d83abdb186413fab3
Syndicate555 <saziz028@uottawa.ca>
2020-07-06 04:52:10 -0400

    changes

--- src/api/api-request.js
+++ src/api/api-request.js
@@ -4,13 +4,15 @@ const router = express.Router();
 const BASE_URL = `http://newsapi.org/v2/everything?`;
 
 router.get("/", (req, res) => {
-  const params = new URLSearchParams({
-    api_key: "API_KEY",
-    domains: "wsj.com",
-  });
-  res.json({
-    message: "Hello from the api",
-  });
+  try {
+    const params = new URLSearchParams({
+      api_key: process.env.API_KEY,
+      domains: "wsj.com",
+    });
+    res.json({
+      message: "Hello from the api",
+    });
+  } catch (error) {}
 });
 
 module.exports = router;
