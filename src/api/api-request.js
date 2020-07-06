f3d300fad75539ccdddf01cc870f8e224b6ed607
Syndicate555 <saziz028@uottawa.ca>
2020-07-06 04:43:42 -0400

    Update api-request.js

--- src/api/api-request.js
+++ src/api/api-request.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const BASE_URL = `http://newsapi.org/v2/everything?domains=wsj.com&apiKey=API_KEY`;
 
 router.get("/", (req, res) => {
-  console.log("connected");
   res.json({
     message: "Hello from the api",
   });
