9201d7bc0244acb8556177f3a4a675ceff17e89d
Syndicate555 <saziz028@uottawa.ca>
2020-07-06 05:02:42 -0400

    Update api-request.js

--- src/api/api-request.js
+++ src/api/api-request.js
@@ -4,6 +4,7 @@ const { default: Axios } = require("axios");
 const router = express.Router();
 const BASE_URL = `http://newsapi.org/v2/everything?`;
 const BASE_URL2 = `http://newsapi.org/v2/everything?domains=wsj.com&apiKey=2e209d65084a4475920191f033f023ce`;
+require("dotenv").config();
 
 router.get("/", async (req, res) => {
   try {
