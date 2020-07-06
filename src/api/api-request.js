f60954ae9ffd8b3246df8c40ffd0f8f03720d6f4
Syndicate555 <saziz028@uottawa.ca>
2020-07-06 12:47:43 -0400

    Update api-request.js

--- src/api/api-request.js
+++ src/api/api-request.js
@@ -7,6 +7,10 @@ const BASE_URL = `http://newsapi.org/v2/everything?`;
 let cachedData;
 let cachedTime;
 
+// My custom API key
+const apiKeys = new Map();
+apiKeys.set("12345", true);
+
 // Define your API restrictions here
 const limiter = rateLimit({
   windowsMs: 30 * 1000, // 15 minutes
@@ -19,30 +23,36 @@ const speedLimiter = slowDown({
   delayMs: 500,
 });
 
-router.get("/", limiter, async (req, res) => {
-  // in memory cache
-  if (cachedTime && cachedTime > Date.now() - 30 * 1000) {
-    return res.json(cachedData);
+router.get(
+  "/",
+  limiter,
+  speedLimiter,
+  (req, res, next) => {},
+  async (req, res) => {
+    // in memory cache
+    if (cachedTime && cachedTime > Date.now() - 30 * 1000) {
+      return res.json(cachedData);
+    }
+    try {
+      const params = new URLSearchParams({
+        apiKey: process.env.API_KEY,
+        domains: "wsj.com",
+      });
+
+      // 1. make a request to the api
+      const { data } = await Axios.get(`${BASE_URL}${params}`);
+
+      // 2. respond to ths request with data from api
+
+      cachedTime = Date.now();
+
+      cachedData = data;
+      data.cacheTime = cachedTime;
+      return res.json(data);
+    } catch (error) {
+      console.log(error);
+    }
   }
-  try {
-    const params = new URLSearchParams({
-      apiKey: process.env.API_KEY,
-      domains: "wsj.com",
-    });
-
-    // 1. make a request to the api
-    const { data } = await Axios.get(`${BASE_URL}${params}`);
-
-    // 2. respond to ths request with data from api
-
-    cachedTime = Date.now();
-
-    cachedData = data;
-    data.cacheTime = cachedTime;
-    return res.json(data);
-  } catch (error) {
-    console.log(error);
-  }
-});
+);
 
 module.exports = router;
